24a2e2686e7a1ba35f6d1be1e4491f32ef39c9bd
jsl <jsl@taric.es>
2012-10-18 11:10:23 +0200

    Completa metadata del empaquetado refs #8182

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
 
 setup(
     name='matchers',
+    description='Custom Hamcrest matchers',
     version='0.1',
-    packages=find_packages(),
+    long_description=README + '\n',
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Testing",
+        "Framework :: pyHamcrest",
+    ],
     install_requires=open('reqs.txt').read().splitlines(),
+    packages=find_packages(),
+    test_suite='nose.collector',
+    url='http://www.taric.es',
+    author='Taric S.A.',
+    author_email='',
 )
