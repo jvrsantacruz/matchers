b87dd537c287c9e242ff3026ac8c57448d210cbb
jsl <jsl@taric.es>
2012-10-22 12:05:17 +0200

    Adelanta la versión del paquete a la 1.12

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
 setup(
     name='matchers',
     description='Custom Hamcrest matchers',
-    version='0.11',
+    version='0.12',
     long_description=README + '\n',
     classifiers=[
         "Programming Language :: Python",
