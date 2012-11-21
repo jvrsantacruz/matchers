d4513b4767d9baefa54ee2823fafad654f959b5d
jsl <jsl@taric.es>
2012-11-21 11:01:44 +0100

    Añade tests para el matcher has_keys

---
+++ tests/test_has_keys.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from hamcrest import *
+
+from matchers import has_keys
+
+
+class TestHasKeys(object):
+    def test_empty_dict(self):
+        assert_that({}, has_keys([]))
+
+    def test_same_key(self):
+        assert_that(dict(key=1), has_keys(['key']))
+
+    def test_same_keys(self):
+        assert_that(dict(key=1, key2=2), has_keys(['key', 'key2']))
+
+    def test_different_key(self):
+        assert_that(dict(key=1), is_not(has_keys(['nokey'])))
+
+    def test_one_of_many_keys(self):
+        assert_that(dict(key=1, key2=2), has_keys(['key']))
+
+    def test_none_of_many_different_keys(self):
+        assert_that(dict(key=1, key2=2), is_not(has_keys([u'ná'])))
+
+    def test_with_keys_defined_in_a_generator(self):
+        assert_that(dict(key=1), has_keys(k for k in ['key']))
+
+    def test_with_matched_object_being_a_pair_sequence(self):
+        assert_that((('key', 1),), has_keys(['key']))
+
+    def test_with_matched_object_being_a_pair_sequence_generator(self):
+        assert_that(((k,v) for k,v in dict(key=1).items()), has_keys(['key']))
+
+    def test_with_matched_object_being_a_dictionary_generator(self):
+        assert_that({k:v for k,v in dict(key=1).items()}, has_keys(['key']))
