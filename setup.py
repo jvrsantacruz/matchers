8aa1ab6486984aea94ae44e8e4228dcc31db329d
jsl <jsl@taric.es>
2013-02-19 13:44:08 +0100

    Completa información del setup.py

--- setup.py
+++ setup.py
@@ -14,18 +14,20 @@ requires = [
 
 setup(
     name='matchers',
-    description='Custom Hamcrest matchers',
+    description='Collection of highly reusable hamcrest matchers',
     version='0.17',
     long_description=README + '\n',
     classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Testing",
         "Framework :: pyHamcrest",
+        "Operating System :: OS Independent",
     ],
     install_requires=requires,
     packages=find_packages(),
     test_suite='nose.collector',
-    url='http://www.taric.es',
+    url='https://github.com/grupotaric/matchers',
+    download_url='https://github.com/grupotaric/matchers/tags',
     author='Taric S.A.',
-    author_email='',
+    author_email='appregs@taric.es',
 )
